5640876c1ab6350016c90ac7aa0884c9940aab15
Alla5735 <kofelatte@gmail.com>
2022-12-02 21:33:06 +0300

    Добавили еще один раздел

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let a;
+switch (a) {
+    case 0: 
+        console.log('0');
+    case null:
+        console.log('null');
+    case undefined:
+            console.log('undefined');
+    default:
+                console.log('default')
+}
+
+
+console.log("4 " + 2)
